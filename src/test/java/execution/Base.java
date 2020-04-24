d42e8dd83d552c847b1a009b52ea913e9f29bd59
Sayeed <ad@gmail.com>
2020-04-23 22:18:24 -0400

    pushing basic

--- src/test/java/execution/Base.java
+++ src/test/java/execution/Base.java
@@ -14,55 +14,49 @@ import org.openqa.selenium.*;
 import org.testng.annotations.*;
 import org.testng.annotations.Parameters;
 
-
 public class Base {
-	public WebDriver driver;
+public WebDriver driver;
 
-	
+
+
 	@BeforeClass
 	@Parameters("browser")
-public void Startup(String browse) throws IOException {
-	if (browse.contentEquals("chrome")) {
-	System.setProperty("webdriver.chrome.driver",System.getProperty("user.dir")+"\\driver\\chromedriver.exe");
-	
-	driver = new ChromeDriver();
+	public void Startup(String browse) throws IOException {
+		if (browse.contentEquals("chrome")) {
+			System.setProperty("webdriver.chrome.driver",
+					System.getProperty("user.dir") + "\\driver\\chromedriver.exe");
 
+			driver = new ChromeDriver();
 
-
-	}
-	else if(browse.contentEquals("firefox")) {
-		System.setProperty("webdriver.gecko.driver",System.getProperty("user.dir")+"\\driver\\geckodriver.exe");
-		driver= new FirefoxDriver();
-	}
-		else {
+		} else if (browse.contentEquals("firefox")) {
+			System.setProperty("webdriver.gecko.driver", System.getProperty("user.dir") + "\\driver\\geckodriver.exe");
+			driver = new FirefoxDriver();
+		} else {
 			System.out.println("please choose right browser");
 		}
-	
-	
-	driver.get("https://admin-demo.nopcommerce.com/admin/");
-	driver.manage().window().maximize();
-	driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
-	driver.findElement(By.id("Email")).sendKeys("admin@yourstore.com");
-	driver.findElement(By.id("Password")).sendKeys("admin");
-	driver.findElement(By.xpath("//input[@type='submit']")).click();
-	
-	}
-	public void screens() throws IOException {
-		
-		String stamp =new SimpleDateFormat("E, MMM-dd-yyyy-HH-mm-ss").format(new Date());
-	    File take =((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
-		
-		FileUtils.copyFile(take,new File(System.getProperty("user.dir") +"\\screenshots\\"+stamp+".png"));
-		
-		
-	}
-	
-	
-	
-}
-	
-	
-	
-	
-	
 
+		driver.get("https://admin-demo.nopcommerce.com/admin/");
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+		driver.findElement(By.id("Email")).sendKeys("admin@yourstore.com");
+		driver.findElement(By.id("Password")).sendKeys("admin");
+		driver.findElement(By.xpath("//input[@type='submit']")).click();
+
+		
+		
+	}
+
+	public void screens(String loc) throws IOException {
+
+		String stamp = new SimpleDateFormat(" E, MMM-dd-yyyy-HH-mm-ss").format(new Date());
+		File take = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+		FileUtils.copyFile(take, new File(System.getProperty("user.dir") + "\\screenshots\\" + loc + stamp + ".png"));
+
+	}
+	
+	@AfterClass
+	public void teardown() {
+		driver.close();
+	}
+
+}
---
+++ src/test/java/execution/POM.java
@@ -0,0 +1,36 @@
+package execution;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class POM extends Base{
+	@FindBy(xpath = "//body[@class='skin-blue sidebar-mini']/div[@class='wrapper']/div[@class='main-sidebar']/div[@class='sidebar']/ul[@class='sidebar-menu tree']/li[9]/a[1]")
+	private WebElement mainHeading;
+
+	@FindBy(xpath = "//span[contains(text(),'Bestsellers')]")
+	private WebElement subHeading;
+
+	@FindBy(xpath = "search-salesreport")
+	private WebElement searchbtn;
+	
+
+
+	public POM(WebDriver driver) {
+		PageFactory.initElements(driver, this);
+	}
+
+	public WebElement mainhd() {
+		return mainHeading;
+	}
+
+	public WebElement subhd() {
+		return subHeading;
+	}
+
+	public WebElement pushsearch() {
+		return searchbtn;
+	}
+
+}
